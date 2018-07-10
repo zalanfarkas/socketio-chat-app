7675609c62bbad72a59323ad1c33aca4f2b1c65b
Zalan Farkas <zalan.farkas96@gmail.com>
2018-07-10 12:23:51 +0200

    Event Acknowledgements

--- public/js/index.js
+++ public/js/index.js
@@ -9,3 +9,10 @@ socket.on("disconnect", function() {
 socket.on("newMessage", function(message){
     console.log("New message", message);
 });
+
+socket.emit("createMessage", {
+    from: "Bella",
+    text: "Hi"
+}, function(data){
+    console.log("Got it", data);
+});
