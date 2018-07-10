e1a447190adc635b452f66ddd0effcba025a7ffa
Zalan Farkas <zalan.farkas96@gmail.com>
2018-07-10 14:58:23 +0200

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-const {generateMessage} = require("./utils/message");
+const {generateMessage, generateLocationMessage} = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 let app = express();
@@ -24,8 +24,12 @@ io.on("connection", (socket) => {
         io.emit("newMessage", generateMessage(message.from, message.text));
         callback("This is from the server");
 
-        
+
         // socket.broadcast.emit("newMessage", generateMessage(message.from, message.text));
+    });
+
+    socket.on("createLocationMessage", (coords) => {
+        io.emit("newLocationMessage", generateLocationMessage("Admin",coords.latitude,coords.longitude));
     });
 
     socket.on("disconnect", () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 let expect = require("expect");
-let {generateMessage} = require("./message");
+let {generateMessage, generateLocationMessage} = require("./message");
 
 describe("generateMessage", () => {
 
@@ -10,6 +10,23 @@ describe("generateMessage", () => {
             text: "Hi"
         });
         expect(result.createdAt).toBeA("number");
+    });
+
+});
+
+describe("generateLocationMessage", () => {
+
+    it("should generate correct location object", () => {
+        let from =  "Zali";
+        let lat = 47.16213550000001;
+        let long = 20.18247120000001;
+        let message = generateLocationMessage(from, lat, long);
+
+        expect(message).toInclude({
+            from,
+            url: `https://www.google.com/maps?q=${lat},${long}`
+        });
+        expect(message.createdAt).toBeA("number");
     });
 
 });
